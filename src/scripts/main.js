fea0e07ef34c365955f677354247675a0836edc7
Максим Щеглов <scheglov@kefir-media.ru>
2017-04-07 17:50:07 +0300

    Блоки на главной странице

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+
+
+    // Бургер
+
+    $('.menu__btn').on("click", function () {
+        $('.menu__navigation').slideToggle();
+        $('.menu__btn').toggleClass('active');
+    });
+
+
+
+
+});
